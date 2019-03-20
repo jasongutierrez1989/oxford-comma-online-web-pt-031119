a48bbf24f104cf205118af936efa72294bc86256
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-03-20 23:00:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-  array.join(", ")
+  if array.length == 2
+    array.join(" and ")  
+  end
+  if array.length == 3
+    array.join(", ")
+    array.insert(3, ' and ')
+  end
+  if array.length > 3
+    array.join(", ")
+    array.insert(array.length - 2, ' and ')
+  end
 end
