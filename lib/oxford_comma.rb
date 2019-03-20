eae13d02d886923fe08b5c892c85776093436547
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-03-20 23:03:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   end
-  if array.length == 3
-    array.join(', ')
-    array.insert(3, ' and ')
-  end
-  if array.length > 3
+  if array.length >= 3
     array.join(', ')
     array.insert(array.length - 1, ' and ')
   end
